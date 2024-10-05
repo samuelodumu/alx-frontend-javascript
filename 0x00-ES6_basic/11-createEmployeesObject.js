d57c1331d4bbe459d97914e2bd84f6ed40ec1fab
Samuel <themainsamuel@gmail.com>
2024-10-06 00:14:15 +0100

    modified task 11

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,6 @@
 export default function createEmployeesObject(departmentName, employees) {
   const employee = {
-    [departmentName]: [employees],
+    [departmentName]: employees,
   };
   return employee;
 }
