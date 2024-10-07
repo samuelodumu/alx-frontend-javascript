6e2ad9660076d5e5b53acecf61259f92bfd451f6
Samuel <themainsamuel@gmail.com>
2024-10-07 07:46:58 +0100

    Got it right now ig

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,10 @@
 export default function handleResponseFromAPI(promise) {
-  console.log('Got a response from the API');
   promise.then(() => (
     {
       status: 200,
       body: 'success',
     }
   ));
-  promise.catch(Error);
+  promise.catch(new Error());
+  promise.finally(() => console.log('Got a response from the API'));
 }
