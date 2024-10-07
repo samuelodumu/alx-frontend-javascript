81cb20be4293f7b01da441cdfa21da6e43fd455a
Samuel <themainsamuel@gmail.com>
2024-10-07 03:41:42 +0100

    modified task 2 and know to always put an object literal in parentheses when returning them in code blocks

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,10 @@
 export default function handleResponseFromAPI(promise) {
   console.log('Got a response from the API');
-  promise.then({
-    status: 200,
-    body: 'Success',
-  });
+  promise.then(() => (
+    {
+      status: 200,
+      body: 'success',
+    }
+  ));
   promise.catch(Error);
 }
