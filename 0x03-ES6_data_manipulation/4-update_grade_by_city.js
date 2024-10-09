f4171df022b655508d49945da36d6e4ea71117eb
Samuel <themainsamuel@gmail.com>
2024-10-09 15:33:22 +0100

    task 4: Combine

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,6 +1,10 @@
 export default function updateStudentGradeByCity(objectsList, city, newGrades) {
   const studentsAtLocation = objectsList.filter((obj) => obj.location === city);
-  const idsList = newGrades.map((obj) => obj.studentId);
-  const studentsWithId = studentsAtLocation.map((obj) => obj.id in idsList);
-  return studentsWithGrade;
+  return studentsAtLocation.map((student) => {
+    const newGrade = newGrades.find((grade) => grade.studentId === student.id);
+    return {
+      ...student,
+      grade: newGrade ? newGrade.grade : 'N/A',
+    };
+  });
 }
