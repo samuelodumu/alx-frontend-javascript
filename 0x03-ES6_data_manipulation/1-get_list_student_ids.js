fe050a90e3a879d2b4198feb0ba8a766d46d85ad
Samuel <themainsamuel@gmail.com>
2024-10-09 08:56:32 +0100

    modified task 1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,10 +1,6 @@
 export default function getListStudentIds(objectsList) {
   if (Array.isArray(objectsList)) {
-    const newMap = new Map(Object.entries(objectsList));
-    const ids = [];
-    for (const x of newMap.values()) {
-      ids.push(x.id);
-    }
+    const ids = objectsList.map((obj) => obj.id);
     return ids;
   }
   return [];
